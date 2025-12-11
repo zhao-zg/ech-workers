dbbc812d56fc1924e23c8f40e6b2b1bb26e10c23
zhao-zg <787854091@qq.com>
2025-12-11 16:15:54 +0800

    修复Android编译错误：添加TextView导入

--- android/src/main/java/com/ech/workers/MainActivity.java
+++ android/src/main/java/com/ech/workers/MainActivity.java
@@ -22,6 +22,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.net.VpnService;
 import android.os.Handler;
